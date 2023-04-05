85d2e91153cf4c2b9a0556146c97eb5cb8468275
unknown <rmmccar92@gmail.com>
2023-04-04 22:41:29 -0400

    so many console logs

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -13,7 +13,7 @@ import { renderToString } from "react-dom/server";
 export default async function handler(req: Request, res: Response) {
   const response = await nowPlaying();
   const data = await response.json();
-  console.log("DATA", data);
+  // console.log("DATA", data);
   // res.status(200).json(data);
   const { item } = data;
   const { is_playing: isPlaying = false, progress_ms: progress = 0 } = data;
