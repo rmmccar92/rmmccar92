b5d150a2e6e3bde9cd7992999f246d2b31d0916f
unknown <rmmccar92@gmail.com>
2023-07-15 12:25:44 -0400

    no idea why that just stopped working? Should be fixed now

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -10,21 +10,28 @@ import {
 
 const getAccessToken = async () => {
   const refresh_token = process.env.REFRESH_TOKEN;
-
-  const response = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-      Authorization: `Basic ${Buffer.from(
-        `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
-      ).toString("base64")}`,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      grant_type: "refresh_token",
-      refresh_token: refresh_token ?? "",
-    }),
-  });
-  return response.json();
+  console.log("REFRESH TOKEN", refresh_token);
+  try {
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+        ).toString("base64")}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: refresh_token ?? "",
+      }),
+    });
+    const result = await response.json();
+    console.log("RESPONSE", result);
+    return result;
+  } catch (e) {
+    console.log("ERROR", e);
+    return {};
+  }
 };
 
 export const topTracks = async () => {
@@ -43,7 +50,7 @@ export const topTracks = async () => {
 
 export const nowPlaying = async () => {
   const { access_token } = await getAccessToken();
-  // console.log("ACCESS TOKEN", access_token);
+  console.log("ACCESS TOKEN", access_token);
 
   return fetch("https://api.spotify.com/v1/me/player/currently-playing", {
     headers: {
